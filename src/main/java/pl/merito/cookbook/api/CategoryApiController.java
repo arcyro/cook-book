1b589a2fb7be2f0b9f14f8504714068e7ae3447a
arcyro <arek@arcyro.pl>
2025-04-08 13:47:03 +0200

    add recipie

---
+++ src/main/java/pl/merito/cookbook/api/CategoryApiController.java
@@ -0,0 +1,29 @@
+package pl.merito.cookbook.api;
+
+import org.springframework.web.bind.annotation.*;
+import pl.merito.cookbook.recipie.Category;
+import pl.merito.cookbook.recipie.CategoryRepository;
+
+@RestController
+@RequestMapping("/api/category")
+public class CategoryApiController {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryApiController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping
+    public Iterable<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+    @GetMapping("/{id}")
+    public Category getCategoryById(@PathVariable Long id) {
+        return categoryRepository.findById(id).orElse(null);
+    }
+    @PostMapping()
+    public Category addCategory(@RequestBody Category category) {
+        return categoryRepository.save(category);
+    }
+}
--- src/main/java/pl/merito/cookbook/recipie/Recipe.java
+++ src/main/java/pl/merito/cookbook/recipie/Recipe.java
@@ -18,4 +18,68 @@ public class Recipe {
     private int preparationTime;
     @ManyToOne
     private Category category;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDateTime getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(LocalDateTime updated) {
+        this.updated = updated;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public int getPreparationTime() {
+        return preparationTime;
+    }
+
+    public void setPreparationTime(int preparationTime) {
+        this.preparationTime = preparationTime;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
